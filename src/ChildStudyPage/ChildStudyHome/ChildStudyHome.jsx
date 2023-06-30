d177aef35a36467ebcc2368d112f7e26253182b9
Kamal raj k <kamalraj5544@gmail.com>
2023-06-30 12:31:48 +0530

    completed childStudy Donate page

--- src/ChildStudyPage/ChildStudyHome/ChildStudyHome.jsx
+++ src/ChildStudyPage/ChildStudyHome/ChildStudyHome.jsx
@@ -1,27 +1,86 @@
-import React from 'react'
+import React from "react";
 
-import cStudyBg from "../../assets/children-syria-earthquake-2023 1.jpg"
+import cStudyBg from "../../assets/children-syria-earthquake-2023 1.jpg";
 
-import MainNavbar from '../../components/MainNavbar/MainNavbar'
-import BackgroundSection from '../../components/BackroundSection/BackgroundSection'
-import ChildCard from '../../components/ChildCard/ChildCard'
+import backStroryChild from "../../assets/pexels-abhay-pratap-14108494 1.jpg";
+
+import MainNavbar from "../../components/MainNavbar/MainNavbar";
+import BackgroundSection from "../../components/BackroundSection/BackgroundSection";
+import ChildCard from "../../components/ChildCard/ChildCard";
+import Footer from "../../components/Footer/Footer";
 
 const ChildStudyHome = () => {
+  const childCardArray = [
+    <ChildCard />,
+    <ChildCard />,
+    <ChildCard />,
+    <ChildCard />,
+    <ChildCard />,
+    <ChildCard />,
+  ];
   return (
-    <div className='childStudy-wrapper'>
-    <header>
+    <div className="childStudy-wrapper">
+      <header>
         <MainNavbar />
-    </header>
+      </header>
 
-    <BackgroundSection image={cStudyBg} contentName="Child Study" />
+      <BackgroundSection image={cStudyBg} contentName="Child Study" />
 
-    <section className='container'>
-    <h2 className='fw-bolder fs-2 text-center text-danger my-5'>Loves Grows by Giving</h2>
+      <section className="container my-5">
+        <h2 className="fw-bolder fs-2 text-center text-danger my-5">
+          Loves Grows by Giving
+        </h2>
 
-    <ChildCard />
-    </section>
+        <div className="row row-cols-3  g-5 container">
+          {childCardArray.map((ele) => (
+            <div className="col">{ele}</div>
+          ))}
+        </div>
+      </section>
+
+      <section>
+        <h2 className="fw-bolder fs-2 text-center text-danger my-5">
+          Our Back Story
+        </h2>
+
+        <div className="row">
+          <div className="col-6">
+            <img
+              src={backStroryChild}
+              alt="childImage"
+              className="w-100 h-100 "
+            />
+          </div>
+          <div className="col-6 d-flex justify-content-center flex-column">
+            <h4 className="fw-bold my-5">Heart Broken Story</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Ullamcorper phasellus
+              accumsan tristique nibh orci pharetra elit felis. Imperdiet in
+              ipsum massa aenean. Lorem ipsum dolor sit amet consectetur.
+              Ullamcorper phasellus accumsan tristique Lorem ipsum dolor sit
+              amet consectetur. Ullamcorper phasellus accumsan tristique nibh
+              orci pharetra elit felis. Imperdiet in ipsum massa aenean. Lorem
+              ipsum dolor sit amet consectetur. Ullamcorper phasellus accumsan
+              tristique Lorem ipsum dolor sit amet consectetur. Ullamcorper
+              phasellus accumsan tristique nibh orci pharetra elit felis.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur. Ullamcorper phasellus
+              accumsan tristique nibh orci pharetra elit felis. Imperdiet in
+              ipsum massa aenean. Lorem ipsum dolor sit amet consectetur.
+              Ullamcorper phasellus accumsan tristique Lorem ipsum dolor sit
+              amet consectetur.
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <section className="footer-section">
+        <Footer />
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default ChildStudyHome
+export default ChildStudyHome;
--- src/components/ChildCard/ChildCard.jsx
+++ src/components/ChildCard/ChildCard.jsx
@@ -4,21 +4,23 @@ import cCardImage from "../../assets/medium-shot-happy-kids-posing 1.jpg";
 
 const ChildCard = () => {
   return (
-    <div className="card" style={{ width: "25rem" }}>
+    <div className="card border-0 bg-body-secondary">
       <img src={cCardImage} className="card-img-top" alt="childImage" />
       <div className="card-body">
         <h5 className="card-title fw-bolder fs-3">Frank Stephan</h5>
-        <p className="card-text "><strong>Age:</strong><span>15</span></p>
+        <p className="card-text ">
+          <strong>Age:</strong>
+          <span>15</span>
+        </p>
         <p className="card-text">
-          Lorem ipsum dolor sit amet consectetur. Ullamcorper phasellus accumsan
-          tristique nibh orci pharetra elit felis. Imperdiet in ipsum massa
-          aenean.
+          Lorem ipsum dolor sit amet consectetur.Ullamcorper phasellus accumsan
+          tristique nibh orci pharetra elit felis.
         </p>
       </div>
       <div className="card-body">
-        <button className="btn btn-danger fs-5 fw-bolder">Donate Now</button>
-        <button className="btn border text-primary ms-3">
-        <i class="bi bi-share"></i>
+        <button className="btn btn-danger fw-bolder">Donate Now</button>
+        <button className="btn border text-primary ms-3 bg-white">
+          <i class="bi bi-share"></i>
         </button>
       </div>
     </div>

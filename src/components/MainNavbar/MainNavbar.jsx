055c2c2720f4bf708c6beb241a6e9d653d344b2b
Kamal raj k <kamalraj5544@gmail.com>
2023-07-03 10:25:30 +0530

    updated navbar

--- src/components/MainNavbar/MainNavbar.jsx
+++ src/components/MainNavbar/MainNavbar.jsx
@@ -1,55 +1,60 @@
 import React from "react";
 
-import "./MainNavbar.css"
+import "./MainNavbar.css";
 
 import { Link } from "react-router-dom";
 
 const MainNavbar = () => {
   return (
-      <nav className="navbar navbar-expand-lg bg-red">
-        <div className="container-fluid bg-red">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo01"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse justify-content-end align-items-center"
-            id="navbarTogglerDemo01"
-          >
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link text-white" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link text-white" href="/aboutUs">
-                  About Us
-                </a>
-              </li>
-              <li className="nav-item ">
-                <a className="nav-link text-white" href="/services">Services</a>
-              </li>
-              <li className="nav-item ">
-                <a className="nav-link text-white" href="/contactUs">Contact Us</a>
-              </li>
-            </ul>
-            <Link to="/login"
-                  className="btn bg-white text-danger fw-bolder mb-3 ms-3"
-                  type="submit"
-                >
+    <nav className="navbar navbar-expand-lg bg-red">
+      <div className="container-fluid bg-red">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse d-flex justify-content-end align-items-center"
+          id="navbarTogglerDemo01"
+        >
+          <ul className="navbar-nav mb-2 mb-lg-0 me-5">
+            <li className="nav-item">
+              <a className="nav-link text-white" aria-current="page" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link text-white" href="/aboutUs">
+                About Us
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link text-white" href="/services">
+                Services
+              </a>
+            </li>
+            <li className="nav-item ">
+              <a className="nav-link text-white" href="/contactUs">
+                Contact Us
+              </a>
+            </li>
+            <li className="nav-item">
+              <Link to="/login" type="submit">
+                <button className="btn bg-white text-danger fw-bolder py-2 display-3">
                   Log In
-                </Link>
-          </div>
+                </button>
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
+      </div>
+    </nav>
   );
 };
 

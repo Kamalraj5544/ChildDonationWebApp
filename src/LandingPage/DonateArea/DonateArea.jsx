66e483f6748984bf3bc3ee70fb2532d1925a7434
Kamal raj k <kamalraj5544@gmail.com>
2023-07-04 16:54:43 +0530

    updated some width and heights

--- src/LandingPage/DonateArea/DonateArea.jsx
+++ src/LandingPage/DonateArea/DonateArea.jsx
@@ -6,11 +6,11 @@ const DonateArea = () => {
   return (
     <div className="donateArea-container text-white ">
       <div className="bg-donateArea"></div>
-      <section className="donateArea-text container d-flex flex-column justify-content-center align-items-center pb-5 text-center">
-        <h3 className="fs-1 fw-bold pt-4">
+      <section className="donateArea-text w-100 h-100 container d-flex flex-column justify-content-center align-items-center pb-5 text-center">
+        <h3 className="fs-1 fw-bolder pt-4">
           Raise funds for a cause. Give Fundraisersis a free platform
         </h3>
-        <p className="fw-normal pt-3">
+        <p className="fw-normal py-4">
           Lorem ipsum dolor sit amet consectetur. Ullamcorper phasellus accumsan
           tristique nibh orci pharetra elit felis. Imperdiet in ipsum massa
           aenean. Lorem ipsum dolor sit amet consectetur. Ullamcorper phasellus

adc306e429f7b4500f9cb4859ca93070fae24881
Kamal raj k <kamalraj5544@gmail.com>
2023-07-10 18:23:49 +0530

    add child post api integration done

--- src/pages/ChildrenList/AddChild/AddChild.jsx
+++ src/pages/ChildrenList/AddChild/AddChild.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import React from "react";
 import { useState } from "react";
 
@@ -8,160 +7,186 @@ import { FiUpload } from "react-icons/fi";
 
 import { useNavigate } from "react-router-dom";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import axios from "axios";
+
 const AddChild = () => {
+
+  const ageInput = document.getElementById('age');
+
+  ageInput.oninvalid = function(event) {
+    event.target.setCustomValidity('Please enter a valid age');
+  }
+
   const [childDetails, setChildDetails] = useState({
-    name:"",
+    name: "",
     addBy: "64a6b4f9ce2e1705901eed29",
+    age: "",
+    description: "",
+    type: "",
+    imageUrl: "",
   });
 
-  const navigate = useNavigate();
 
   const handlePostChildData = async () => {
     await axios
       .post(
         `${apiBaseUrl}/neethimaan/createChildrenList?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbklkIjoiNjRhNmI0ZjljZTJlMTcwNTkwMWVlZDI5IiwiaWF0IjoxNjg4NjQ3MDYzfQ.kfwPj9B43M7MIGfxCtdJY5R7UjmW0aF0Jq5Qs3aBKfI`,
-        childDetails,
-        {
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-        }
+        childDetails
       )
       .then((res) => {
         console.log(res);
         console.log(res.data);
+        res.status === 200
+          ? toast.success("Successfully added!", {
+              position: toast.POSITION.TOP_RIGHT,
+            })
+          : toast.error("Something went wrong!", {
+              position: toast.POSITION.TOP_RIGHT,
+            });
       });
 
-    navigate("/admin/childrenList");
+    // navigate("/admin/childrenList");
   };
   return (
     <div className="d-flex container flex-column border rounded p-4">
       <h4 className="text-danger fw-bold">Add Children Details</h4>
       <hr />
 
-      <div className="py-3 row1">
-        <div className="row">
-          <div className="col">
-            <label for="firstName" className="fw-bold pb-2">
-              First Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter your First name"
-              aria-label="First name"
-              id="firstName"
-              onChange={(e) =>
-                setChildDetails({
-                  ...childDetails,
-                  name: e.target.value
-                })
-              }
-            />
-          </div>
-          <div className="col">
-            <label for="lastName" className="fw-bold pb-2">
-              Last Name
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Enter your Last name"
-              aria-label="Last name"
-              id="LastName"
-              
-            />
+      <ToastContainer />
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handlePostChildData();
+        }}
+      >
+        <div className="py-3 row1">
+          <div className="row">
+            <div className="col">
+              <label for="firstName" className="fw-bold pb-2">
+                First Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your First name"
+                aria-label="First name"
+                id="firstName"
+                required
+                onChange={(e) =>
+                  setChildDetails({
+                    ...childDetails,
+                    name: e.target.value,
+                  })
+                }
+              />
+            </div>
+            <div className="col">
+              <label for="lastName" className="fw-bold pb-2">
+                Last Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your Last name"
+                aria-label="Last name"
+                id="LastName"
+                pattern="/^[a-zA-Z]{2,40}([a-zA-Z]{2,40})+$/"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="pb-4 row2">
-        <div className="row">
-          <div className="col">
-            <label for="age" className="fw-bold pb-2">
-              Age
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Age"
-              aria-label="Age"
-              id="age"
-              onChange={(e) =>
-                setChildDetails({ ...childDetails, age: +e.target.value })
-              }
-            />
-          </div>
-          <div className="col">
-            <label for="category" className="fw-bold pb-2">
-              Category
-            </label>
-            <select
-              className="form-select"
-              aria-label="Default select example"
-              id="category"
-              onChange={(e) =>
-                setChildDetails({ ...childDetails, type: e.target.value })
-              }
-            >
-              <option selected>Select </option>
-              <option value="Child study">Child Study</option>
-              <option value="Child health">Child Health</option>
-              <option value="Free Legal">Free Legal</option>
-            </select>
+        <div className="pb-4 row2">
+          <div className="row">
+            <div className="col">
+              <label for="age" className="fw-bold pb-2">
+                Age
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Age"
+                aria-label="Age"
+                id="age"
+                pattern="[0-9]{1,2}"
+                required
+                onChange={(e) =>
+                  setChildDetails({ ...childDetails, age: e.target.value })
+                }
+              />
+            </div>
+            <div className="col">
+              <label for="category" className="fw-bold pb-2">
+                Category
+              </label>
+              <select
+                className="form-select"
+                aria-label="Default select example"
+                id="category"
+                required
+                onChange={(e) =>
+                  setChildDetails({ ...childDetails, type: e.target.value })
+                }
+              >
+                <option selected>Select </option>
+                <option value="Child study">Child Study</option>
+                <option value="Child health">Child Health</option>
+                <option value="Free Legal">Free Legal</option>
+              </select>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="row3 pb-4">
-        <label for="imgUpload" className="fw-bold pb-2 d-block">
-          Image Upload
-        </label>
-        <button
-          id="imgUpload"
-          className="btn border bg-dark text-white fw-bold d-flex flex-row"
-        >
-          <span className="mt-2 me-2">
-            <FiUpload />
-          </span>
-          <input
-            class="form-control"
-            type="file"
+        <div className="row3 pb-4">
+          <label for="imgUpload" className="fw-bold pb-2 d-block">
+            Image Upload
+          </label>
+          <button
+            id="imgUpload"
+            className="btn border bg-dark text-white fw-bold d-flex flex-row"
+          >
+            <span className="mt-2 me-2">
+              <FiUpload />
+            </span>
+            <input
+              class="form-control"
+              type="file"
+              onChange={(e) =>
+                setChildDetails({ ...childDetails, imageUrl: e.target.value })
+              }
+            />
+          </button>
+        </div>
+
+        <div className="row4 pb-3">
+          <label for="description" className="fw-bold pb-2 d-block">
+            Description
+          </label>
+          <textarea
+            className="form-control"
+            id="description"
+            rows="4"
+            placeholder="Type"
+            required
             onChange={(e) =>
-              setChildDetails({ ...childDetails, imageUrl: e.target.value })
+              setChildDetails({ ...childDetails, description: e.target.value })
             }
-          />
-        </button>
-      </div>
+          ></textarea>
+        </div>
 
-      <div className="row4 pb-3">
-        <label for="description" className="fw-bold pb-2 d-block">
-          Description
-        </label>
-        <textarea
-          className="form-control"
-          id="description"
-          rows="4"
-          placeholder="Type"
-          onChange={(e) =>
-            setChildDetails({ ...childDetails, description: e.target.value })
-          }
-        ></textarea>
-      </div>
-
-      <div className="row5">
-        <button className="btn btn-outline-danger me-3" type="button">
-          Cancel
-        </button>
-        <button
-          className="btn btn-danger"
-          type="button"
-          onClick={() => handlePostChildData()}
-        >
-          Update
-        </button>
-      </div>
+        <div className="row5">
+          <button className="btn btn-outline-danger me-3" type="button">
+            Cancel
+          </button>
+          <button className="btn btn-danger" type="submit">
+            Update
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
